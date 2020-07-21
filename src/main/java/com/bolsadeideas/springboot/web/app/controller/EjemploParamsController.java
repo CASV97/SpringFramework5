9651cede16d16facb2cc31591024e33e69f3b951
Christan Ariel Salazar Verduga <santisimoariel@gmail.com>
2020-07-21 20:30:32 +0200

    Obtener varios parámetros de la URL con @RequestParam

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +34,40 @@ public class EjemploParamsController {
 	public String param(@RequestParam(required = false, defaultValue = "algún valor") String texto, Model model) {
 		model.addAttribute("titulo", "Recibir parámetros del request HTTP GET - URL");
 		model.addAttribute("resultado", "el texto enviado es: " + texto);
+		return "/params/ver";
+	}
+
+	/*
+	 * vamos a ver como enviar más de un parámetro en la url, podemos enviar
+	 * parámetros de cualquier tipo básico de java, pero lo mas típico es mandar un
+	 * String, Integer o long
+	 */
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		model.addAttribute("titulo", "Recibir parámetros del request HTTP GET - URL");
+		model.addAttribute("resultado", "el saludo enviado es: '" + saludo + "' y el número es: '" + numero + "'");
+		return "/params/ver";
+	}
+
+	/**
+	 * otra forma de recibir parámetros en el controlador, pero esta vez en vez de
+	 * utilizar el <code> @RequestParam </code>, podemos pasar el objeto
+	 * <code>HttpServletRequest</code> directamente
+	 */
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		// conviene cada vez que se usa un Parse.. conviene usarlo dentro de un bloque
+		// try...cath para problemas de conversiones
+		Integer numero = null;
+		try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+
+		} catch (NumberFormatException e) {
+			numero = 0;
+		}
+		model.addAttribute("titulo", "Recibir parámetros del request HTTP GET - URL");
+		model.addAttribute("resultado", "el saludo enviado es: '" + saludo + "' y el número es: '" + numero + "'");
 		return "/params/ver";
 	}
 
