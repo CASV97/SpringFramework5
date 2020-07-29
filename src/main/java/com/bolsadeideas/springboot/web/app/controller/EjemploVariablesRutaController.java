5962b80b3c2593598f2c393d7d142dc4ac440642
Christan Ariel Salazar Verduga <santisimoariel@gmail.com>
2020-07-29 17:17:57 +0200

    Obteniendo varios parámetros de la ruta con @PathVarieble

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploVariablesRutaController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploVariablesRutaController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/variables")
 public class EjemploVariablesRutaController {
+	@GetMapping("/")
+	public String index (Model model) {
+		model.addAttribute("titulo", "Enviar parámetros de la ruta (@PathVariable)");
+		return "variables/index";
+	}
 	/*
 	 * la parte dinámica de la ruta va entre llave en este caso "texto", e
 	 * importante que el nombre del parámetro sea igual
@@ -25,4 +30,13 @@ public class EjemploVariablesRutaController {
 
 		return "variables/ver";
 	}
+	/*Como enviar y obtener varias variables o parámetros de la ruta */
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable String texto,@PathVariable Integer numero, Model model) {
+		model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable)");
+		model.addAttribute("resultado", "el texto de la ruta es: " + texto+" y  el número es: "+numero);
+
+		return "variables/ver";
+	}
+
 }
